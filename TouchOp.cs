21799801c32a563680f8e404dc382752f3fc9b8b
LIyou <1085066875@qq.com>
2018-11-02 16:52:26 +0800

    Add files via upload

---
+++ TouchOp.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchOp : MonoBehaviour {
+
+    private float pinchZoomSensitivity = 0.5f;
+    private float MoveSpeed =0.2f;//平移灵敏度
+    private float yawSensitivity = 10.0f;
+    private float pitchSensitivity = 12.0f;
+    private float DistanceBetweenCamAndRef = 10.0f;
+    public GameObject LookAtGameObject;
+    void Start()
+    {
+        Screen.fullScreen = true;
+        Screen.SetResolution(1920, 1080, true);
+        InitialRotationOperation(LookAtGameObject);
+    }
+    #region Gesture Event Messages
+
+    float nextDragTime = 0.0f;
+    void OnDrag( DragGesture gesture )
+    {
+        
+        if (gesture.Fingers.Count == 1)
+        {
+            Ray ray = Camera.main.ScreenPointToRay (Vec2toVec3(gesture.StartPosition));  //摄像机需要设置MainCamera的Tag这里才能找到  
+            RaycastHit hitInfo;
+            if (Physics.Raycast (ray, out hitInfo)) {  
+                Vector3 hitPoint = hitInfo.point;//返回的是世界坐标系的坐标  
+
+            } 
+            Vector3 temp = hitInfo.point-this.transform.position;
+            Vector3 ReferencePoint = this.transform.position + transform.forward * temp.magnitude;
+            Vector3 DIS = ReferencePoint- this.transform.position;
+            DistanceBetweenCamAndRef = DIS.magnitude;
+
+            float mX = this.transform.eulerAngles.y;
+            Debug.Log("mX is : " + mX.ToString());
+            float mY = this.transform.eulerAngles.x;
+            Debug.Log("mY is : " + mY.ToString());
+            mX += gesture.DeltaMove.x.Centimeters() * yawSensitivity ;
+            mY -= gesture.DeltaMove.y.Centimeters()* pitchSensitivity ;
+
+            // control the mX or mY between -180 and 180
+            while (mX >= 180)
+            {
+                mX -= 360;
+            }
+            while (mX < -180)
+            {
+                mX += 360;
+            }
+            while (mY >= 180)
+            {
+                mY -= 360;
+            }
+            while (mY < -180)
+            {
+                mY += 360;
+            }
+          // this is the bug of unity ? the following code is the tem solution
+            if(mY>90.0f)
+            {
+                mY = 90.0f;
+
+            }
+            if(mY<-90.0f)
+            {
+                mY = -90.0f;
+            }
+                
+            Quaternion mRotation = Quaternion.Euler(mY,mX, 0);//返回四元数 没有问题
+            if( Time.time < nextDragTime )
+                return;
+            Debug.Log(mRotation);
+            this.transform.position = ReferencePoint;
+            //this.transform.localPosition= Vector3.Lerp(transform.localPosition, transform.position + mRotation * Vector3.back*DistanceBetweenCamAndRef,Time.deltaTime*smoothtime);
+            this.transform.position += mRotation * Vector3.back*DistanceBetweenCamAndRef;
+            //this.transform.position = tempPosition;
+            this.transform.rotation =  mRotation;
+           
+        }
+        else
+            return;
+      
+    }
+
+    void OnPinch( PinchGesture gesture )
+    {
+        if (gesture.Fingers.Count == 2)
+        {
+            float idealDistance = gesture.Delta.Centimeters() * pinchZoomSensitivity;
+            Ray ray = Camera.main.ScreenPointToRay(Vec2toVec3(gesture.StartPosition));  //放大缩小
+            //Vector3.SmoothDamp(transform.position, transform.position + idealDistance * ray.direction, ref cameraVelocity, smoothtime);
+            this.transform.position += idealDistance * ray.direction;
+            nextDragTime = Time.time + 0.25f;  
+        }
+        else
+            return;
+
+    }
+
+    void OnTwoFingerDrag( DragGesture gesture )  //平移
+    {
+        if (gesture.Fingers.Count == 2)
+        {
+            Vector3 ScreenPosition = Vec2toVec3(gesture.StartPosition);
+            Ray ray = Camera.main.ScreenPointToRay(ScreenPosition);  //摄像机需要设置MainCamera的Tag这里才能找到  
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {  
+
+                Vector3 hitPoint = hitInfo.point;//返回的是世界坐标系的坐标 
+                float distem = (hitInfo.point - this.transform.position).magnitude;
+                if (distem < 8.0f)
+                {
+                    MoveSpeed = 0.2f;
+                }
+                else
+                {
+                    if (distem < 30.0f)
+                    {
+                        MoveSpeed = 2.0f;
+                    }
+                    else
+                    {
+                        MoveSpeed = 5.0f;
+                    }
+                }
+
+            }
+            else
+            {
+                MoveSpeed = 1.5f;
+            }
+            Vector3 move = -MoveSpeed * ( transform.right*gesture.DeltaMove.x.Centimeters()+  transform.up*gesture.DeltaMove.y.Centimeters());
+            this.transform.position += move;
+            nextDragTime = Time.time + 0.25f;
+        }
+        else
+            return;
+
+    }
+
+    #endregion
+
+
+    /// <summary>
+    /// convert the type of vector2 to the type of vector3
+    /// </summary>
+    /// <returns>The vec3.</returns>
+    /// <param name="Vec2">Vec2.</param>
+    private Vector3 Vec2toVec3(Vector2 Vec2)
+    {
+        return new Vector3(Vec2.x, Vec2.y, 0.0f);
+    }
+        
+    private void InitialRotationOperation(GameObject ro)//用来初始化带旋转物体的观察状态
+    {
+        float distance = 25.0f;
+        this.transform.position = ro.transform.position+ new Vector3(-10.0f,10.0f,-10.0f);
+        this.transform.rotation = Quaternion.LookRotation (ro.transform.position-this.transform.position);//要减去当前位置
+        this.transform.position = ro.transform.position;
+        this.transform.position += this.transform.rotation * Vector3.back*distance;
+    }
+}
---
+++ maxCamera.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/3dsMax Camera Style")]
+public class maxCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 targetOffset;
+    public float distance = 5.0f;
+    public float maxDistance = 20;
+    public float minDistance = .6f;
+    public float xSpeed = 200.0f;
+    public float ySpeed = 200.0f;
+    public int yMinLimit = -80;
+    public int yMaxLimit = 80;
+    public int zoomRate = 40;
+    public float panSpeed = 0.3f;
+    public float zoomDampening = 5.0f;
+
+    private float xDeg = 0.0f;
+    private float yDeg = 0.0f;
+    private float currentDistance;
+    private float desiredDistance;
+    private Quaternion currentRotation;
+    private Quaternion desiredRotation;
+    private Quaternion rotation;
+    private Vector3 position;
+
+    private Vector3 FirstPosition;
+    private Vector3 SecondPosition;
+    private Vector3 delta;
+    private Vector3 lastOffset;
+    private Vector3 lastOffsettemp;
+    //private Vector3 CameraPosition;
+    //private Vector3 Targetposition;
+    //private Vector3 MoveDistance;
+
+
+    void Start() { Init(); }
+    void OnEnable() { Init(); }
+
+    public void Init()
+    {
+        //If there is no target, create a temporary target at 'distance' from the cameras current viewpoint
+        if (!target)
+        {
+            GameObject go = new GameObject("Cam Target");
+            go.transform.position = transform.position + (transform.forward * distance);
+            target = go.transform;
+        }
+
+        distance = Vector3.Distance(transform.position, target.position);
+        currentDistance = distance;
+        desiredDistance = distance;
+
+        //be sure to grab the current rotations as starting points.
+        position = transform.position;
+        rotation = transform.rotation;
+        currentRotation = transform.rotation;
+        desiredRotation = transform.rotation;
+
+        xDeg = Vector3.Angle(Vector3.right, transform.right);
+        yDeg = Vector3.Angle(Vector3.up, transform.up);
+    }
+
+    /*
+      * Camera logic on LateUpdate to only update after all character movement logic has been handled. 
+      */
+    void LateUpdate()
+    {
+        // If Control and Alt and Middle button? ZOOM!
+        if (Input.GetMouseButton(2))
+        {
+            desiredDistance -= Input.GetAxis("Mouse Y") * Time.deltaTime * zoomRate * 0.125f * Mathf.Abs(desiredDistance);
+        }
+        // If middle mouse and left alt are selected? ORBIT
+        if (Input.GetMouseButton(0))
+        {
+            xDeg += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            yDeg -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            ////////OrbitAngle
+
+            //Clamp the vertical axis for the orbit
+            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
+            // set camera rotation 
+
+
+        }
+        desiredRotation = Quaternion.Euler(yDeg, xDeg, 0);
+        currentRotation = transform.rotation;
+        rotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * zoomDampening);
+        transform.rotation = rotation;
+
+        // otherwise if middle mouse is selected, we pan by way of transforming the target in screenspace
+        if (Input.GetMouseButton(2))
+        {
+            //grab the rotation of the camera so we can move in a psuedo local XY space
+//            target.rotation = transform.rotation;
+//            target.Translate(Vector3.right * -Input.GetAxis("Mouse X") * panSpeed);
+//            target.Translate(transform.up * -Input.GetAxis("Mouse Y") * panSpeed, Space.World);
+        }
+        if (Input.GetMouseButtonDown (1)) //右键平移
+        {
+            FirstPosition = Input.mousePosition;
+            lastOffset = targetOffset;
+        }
+
+        if (Input.GetMouseButton (1)) //右键键平移
+        {
+            SecondPosition = Input.mousePosition;
+            delta = SecondPosition - FirstPosition;
+            targetOffset = lastOffset + transform.right * delta.x*0.03f + transform.up * delta.y*0.03f;
+
+        }
+
+        ////////Orbit Position
+
+        // affect the desired Zoom distance if we roll the scrollwheel
+        desiredDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs(desiredDistance);
+        desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
+        currentDistance = Mathf.Lerp(currentDistance, desiredDistance, Time.deltaTime * zoomDampening);
+
+        position = target.position - (rotation * Vector3.forward * currentDistance );
+
+        position = position - targetOffset;  
+
+        transform.position = position;
+       
+
+       
+       
+    }
+    private Vector3 Vec2toVec3(Vector2 Vec2)
+    {
+        return new Vector3(Vec2.x, Vec2.y, 0.0f);
+    }
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
